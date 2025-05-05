4bc3cf81f3ad3117bc6e304731265d2a520fa4d7
Ghatan55 <ghatanhardanni@gmail.com>
2025-05-05 15:32:55 +0700

    menambahkan pointer deferences

--- PointerSatu.cpp
+++ PointerSatu.cpp
@@ -8,3 +8,18 @@ class mahasiswa{
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main (){
+    mahasiswa mhs; // object mhs
+    mhs.nim = 5;
+    mhs.ShowNim(); // member access Operator
+
+    mahasiswa &refmhs = mhs; // pointer reference refmhs
+    refmhs.nim = 2; // member access operator
+    mhs.ShowNim();
+
+    mahasiswa *pmhs = &mhs; // pointer derefences pmhs
+    pmhs->nim = 3; // arrow operator
+    pmhs->ShowNim();
+    return 0;
+}
