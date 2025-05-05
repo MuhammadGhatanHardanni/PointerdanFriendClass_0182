455e7419a0b46a81141e8532ab4adda0a37a3b3f
Ghatan55 <ghatanhardanni@gmail.com>
2025-05-05 15:23:39 +0700

    Menambahkan pinter untuk memunculkan memori

--- Pointer.cpp
+++ Pointer.cpp
@@ -10,5 +10,34 @@ class Dosen{
 };
 class Satf{
     public:
-    int main;
+    int nidn;
 };
+int main(){
+    Dosen ds;
+    ds.nama = "Giga";
+    ds.tampilNama();
+
+    Dosen &dsref = ds;
+    dsref.nama = "Joko";
+    cout << "Alamat memori = " << &dsref << endl;
+    dsref.tampilNama();
+
+    Dosen *pds = &ds;
+    pds->nama = "Reza";
+    cout << "Alamat Memori = " << pds << endl;
+    pds->tampilNama();
+    
+    int a = 5;
+    int b= 3;
+    int *c = &a;
+    *c = 9;
+    cout << endl;
+    cout << a << endl;
+
+    cout << "Alamat memori a = " << &a << endl;
+    cout << "Alamat memori c = " << c << endl;
+    cout << "Cetak c = " << *c << endl;
+    c = &b;
+    cout << "Alamat Memori c = " << c << endl;
+    cout << "Cetak c  = " << *c << endl;
+}
